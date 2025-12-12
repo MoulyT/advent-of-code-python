bdc26ccebab23109d0bc873f3f64e79bee1333b5
Mouly Taha Sarkadi <moulytaha@z1.digital>
2025-12-12 22:42:28 +0100

    generate statements

---
+++ 2025/4/day04.py
@@ -0,0 +1,14 @@
+# Advent of code Year 2025 Day 4 solution
+# Author = Mouly Taha
+# Date = December 2025
+
+from pathlib import Path
+
+input_file_path = Path(__file__).parent / "input.txt"
+with open(input_file_path, encoding="utf-8") as input_file:
+    input_data = input_file.read()
+
+
+print("Part One :", str(None))
+
+print("Part Two :", str(None))
---
+++ 2025/8/test_day08.py
@@ -0,0 +1,11 @@
+EXAMPLE_INPUT = """"""
+
+
+def test_part_one():
+    """Test part one with example input."""
+    assert True
+
+
+def test_part_two():
+    """Test part two with example input."""
+    assert True
